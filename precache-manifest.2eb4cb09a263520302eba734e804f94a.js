e9c513893a7faa88c448efb7124ee09f95980dd8
Don Shammah <donshammah1@gmail.com>
2024-09-20 17:39:20 +0300

    Updates

---
+++ precache-manifest.2eb4cb09a263520302eba734e804f94a.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6c74d863a5013dd4c8e9c0e6e9044a0d",
+    "url": "/SmartRetailDB/index.html"
+  },
+  {
+    "revision": "1eae3638e8bcdf304d9e",
+    "url": "/SmartRetailDB/static/css/main.b7fb27b2.chunk.css"
+  },
+  {
+    "revision": "c0ee305eb3145ecf5a2b",
+    "url": "/SmartRetailDB/static/js/2.07b41d29.chunk.js"
+  },
+  {
+    "revision": "74e46b9002ec049c1c32",
+    "url": "/SmartRetailDB/static/js/3.835831a2.chunk.js"
+  },
+  {
+    "revision": "1eae3638e8bcdf304d9e",
+    "url": "/SmartRetailDB/static/js/main.1c0b1fa8.chunk.js"
+  },
+  {
+    "revision": "a490e4621fa1d65dafca",
+    "url": "/SmartRetailDB/static/js/runtime~main.1935d7a9.js"
+  }
+]);
